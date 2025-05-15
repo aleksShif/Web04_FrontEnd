941898da723f05e690145b9bd43c8a9a2c1fb6b8
Sasha Shifrina <sasha.shifrina05@gmail.com>
2025-05-14 21:57:30 -0400

    finished styling for forms - adding and editing campuses and students

--- src/components/views/EditCampusView.js
+++ src/components/views/EditCampusView.js
@@ -6,73 +6,46 @@ It constructs a React component to display the new campus page.
 ================================================== */
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 
-// Create styling for the input form
-const useStyles = makeStyles( () => ({
-  formContainer:{  
-    width: '500px',
-    backgroundColor: '#f0f0f5',
-    borderRadius: '5px',
-    margin: 'auto',
-  },
-  title: {
-    flexGrow: 1,
-    textAlign: 'left',
-    textDecoration: 'none'
-  }, 
-  customizeAppBar:{
-    backgroundColor: '#11153e',
-    shadows: ['none'],
-  },
-  formTitle:{
-    backgroundColor:'#c5c8d6',
-    marginBottom: '15px',
-    textAlign: 'center',
-    borderRadius: '5px 5px 0px 0px',
-    padding: '3px'
-  },
-}));
 
 const EditCampusView = (props) => {
   const {handleChange, handleSubmit } = props;
-  const classes = useStyles();
 
   // Render a New Student view with an input form
   return (
     <div>
       <h1>Edit Campus</h1>
 
-      <div className={classes.root}>
-        <div className={classes.formContainer}>
-          <div className={classes.formTitle}>
+      <div>
+        <div className="form-container">
+          <div className="form-title">
             <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
               Edit a Campus
             </Typography>
           </div>
           <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
+          <div className="form-row">
             <label style= {{color:'#11153e', fontWeight: 'bold'}}>Name: </label>
             <input type="text" name="name" onChange ={(e) => handleChange(e)} />
-            <br/>
-            <br/>
+            </div>
 
-            <label style={{color:'#11153e', fontWeight: 'bold'}}>Address: </label>
-            <input type="text" name="address" onChange={(e) => handleChange(e)} />
-            <br/>
-            <br/>
+            <div className="form-row">
+              <label style={{color:'#11153e', fontWeight: 'bold'}}>Address: </label>
+              <input type="text" name="address" onChange={(e) => handleChange(e)} />
+            </div>
 
-            <label style ={{color:'#11153e', fontWeight: 'bold'}}>Description: </label>
-            <input type="text" name="description" onChange={(e) => handleChange(e)} />
-            <br/>
-            <br/>
+            <div className="form-row">
+              <label style ={{color:'#11153e', fontWeight: 'bold'}}>Description: </label>
+              <input type="text" name="description" onChange={(e) => handleChange(e)} />
+            </div>
             {/* <label style={{color:'#11153e', fontWeight: 'bold'}}>Campus Id: </label>
             <input type="text" name="campusId" onChange={(e) => handleChange(e)} />
             <br/>
             <br/> */}
 
-            <Button variant="contained" color="primary" type="submit">
+            <button type="submit">
               Submit
-            </Button>
+            </button>
             <br/>
             <br/>
           </form>
